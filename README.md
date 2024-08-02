f1eec9786bbbbdec3655c15a3b158072b290d57d
NuclearGeek <spickett84@yahoo.com>
2024-08-02 13:56:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Nuclear Geek's Flux Capacitor
+
+A sophisticated project utilizing Black Forest Lab's FLUX model for generating images via a web application.
+
+## Prerequisites
+
+1. **Hugging Face Access Token**:
+   - You need to request access to the FLUX model: [Black Forest Labs](https://huggingface.co/black-forest-labs/FLUX.1-dev)
+   - Once you have access, obtain your Hugging Face token.
+
+2. **Environment Setup**:
+   - Add your Hugging Face token to an `.env` file at the root of the project. Your `.env` file should look like this:
+     ```
+     HF_TOKEN=your_token_here
+     ```
+
+## Getting Started
+
+Follow these steps to set up the environment and run the application:
+
+1. **Run the setup script**:
+   - Execute `start_flux.bat` to install the necessary environment and dependencies, and to run the script.
+   ```plaintext
+   ./start_flux.bat
---
+++ main.py
@@ -0,0 +1,105 @@
+# First run these commands if using Windows and installing manually:
+#
+# pip install git+https://github.com/huggingface/diffusers.git
+# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# pip install python-dotenv transformers accelerate sentencepiece protobuf optimum-quanto gradio
+
+import os
+import torch
+import gradio as gr
+from diffusers import FluxPipeline
+from huggingface_hub import login
+from optimum.quanto import freeze, qfloat8, quantize
+from dotenv import load_dotenv
+
+load_dotenv()
+
+hk_token = os.getenv('HF_TOKEN')
+
+login(token=hk_token)
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+
+# save some VRAM by offloading the model to CPU, disable this if you have enough gpu power
+pipe.enable_model_cpu_offload() 
+
+# Memory-efficient Diffusion Transformers with Quanto and Diffusers
+# https://huggingface.co/blog/quanto-diffusers
+print("Running transformer quantize")
+quantize(pipe.transformer, weights=qfloat8)
+print("Running transformer freeze")
+freeze(pipe.transformer)
+print("Running text_encoder quantize")
+quantize(pipe.text_encoder, weights=qfloat8)
+print("Running text_encoder freeze")
+freeze(pipe.text_encoder)
+
+# Generate Image
+def gen_image(prompt, steps, height, width, seed, guidance_scale):
+    print("Generating...")
+    image = pipe(
+        prompt,
+        height=int(height),
+        width=int(width),
+        guidance_scale=int(guidance_scale),
+        output_type="pil",
+        num_inference_steps=int(steps),
+        max_sequence_length=512,
+        generator=torch.Generator("cuda").manual_seed(int(seed))
+    ).images[0]
+    print("Saving...")
+    return image
+    # image.save(f"{prompt}.png")
+
+with gr.Blocks(theme=gr.themes.Soft(), title="Nuke's Flux Capacitor") as demo:
+    gr.Markdown(f"<h1 style='text-align: center; display:block'>{'Nuke&apos;s Flux Capacitor'}</h1>")
+    with gr.Row():
+
+        steps_slider = gr.Slider(
+            0,100,
+            label = "Steps",
+            value = 50,
+            render = False
+        )
+
+        height_slider = gr.Slider(
+            0,2048,
+            label = "Height",
+            value = 1024,
+            render = False
+        )
+
+        width_slider = gr.Slider(
+            0,2048,
+            label = "Width",
+            value = 1024,
+            render = False
+        )
+
+        seed_slider = gr.Slider(
+            0,99999999999,
+            label = "Seed",
+            value = 0,
+            render = False
+        )
+
+        guidance_slider = gr.Slider(
+            0,20,
+            label = "Guidance Scale",
+            value = 3.5,
+            render = False
+        )
+
+    chat = gr.Interface(
+        fn = gen_image,
+        inputs = [gr.Text(label="Input Prompt"), steps_slider, height_slider, width_slider, seed_slider, guidance_slider], 
+        outputs=[gr.Image(type="numpy", label="Output Image")]
+    )
+
+if __name__ == "__main__":
+    demo.queue()
+    # # Toggle this on if you want to share your app, change the username and password
+    # demo.launch(server_port=7862, share=True, auth=("nuke", "password"))
+
+    # Toggle this on if you want to only run local
+    demo.launch()
